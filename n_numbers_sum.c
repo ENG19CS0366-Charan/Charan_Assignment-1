aec39d9424c393d6ccb8b5f954774d3ddbed0cac
ENG19CS0366-Charan <76121422+ENG19CS0366-Charan@users.noreply.github.com>
2021-04-01 07:57:39 -0400

    Update n_numbers_sum.c

--- n_numbers_sum.c
+++ n_numbers_sum.c
@@ -13,3 +13,38 @@ sum=sum+num;
 printf("sum of %d numbers are:-%d",n,sum);
 return 0;
 }
+
+
+
+//USING FUNCTIONS//
+
+#include<stdio.h>
+int input(int n,int a[])
+{
+    int s=0;
+    for(int i=0;i<n;i++)
+{
+    printf("enter number%d\n",i+1);
+    scanf("%d",&a[i]);
+    s=s+a[i];
+}
+return s;
+}
+void output(int n,int sum,int a[])
+{
+    for(int i=0;i<n-1;i++)
+{
+    printf("%d+",a[i]);
+}
+printf("%d=%d",a[n-1],sum);
+}
+int main()
+{
+    int n,sum=0,i,a[50];
+    printf("enter value of n\n");
+    scanf("%d",&n);
+    sum=input(n,a);
+    output(n,sum,a);
+    return 0;
+}
+
